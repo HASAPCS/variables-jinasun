2c98b1830e00651aa7a04a6f3c7071f718bf560d
jinasun <ginasuns60916@gmail.com>
2023-09-11 12:32:00 +0800

    solution

--- src/main/java/com/example/VariablesAssignment.java
+++ src/main/java/com/example/VariablesAssignment.java
@@ -3,44 +3,50 @@ package com.example;
 public class VariablesAssignment {
 
     // TODO: Declare and initialize a byte variable with any value between 1 and 127
-    byte byteVariable;
+    byte byteVariable = 42;
 
-    // TODO: Declare and initialize a short variable with any value between 1 and 32767
-    short shortVariable;
+    //TODO: Declare and initialize a short variable with any value between 1 and 32767
+    short shortVariable = 1000;
 
     // TODO: Declare and initialize an int variable with any positive value
-    int intVariable;
+    int intVariable = 45;
+
 
     // TODO: Declare and initialize a long variable with any positive value
-    long longVariable;
+    long longVariable = 78890;
 
     // TODO: Declare and initialize a float variable with any positive value
-    float floatVariable;
+    float floatVariable = 89.87;
 
     // TODO: Declare and initialize a double variable with any positive value
-    double doubleVariable;
+    double doubleVariable = 89.87;
 
     // TODO: Declare and initialize a char variable with the initial of your first name
-    char charVariable;
+    char charVariable = "Gina";
 
-    // TODO: Declare and initialize a boolean variable with the value true or false
-    boolean booleanVariable;
+    // TODO: Declare and initialize a boolean variable(true/false) with the value true or false
+    boolean booleanVariablet = true;
+    boolean booleanVariablef = false;
+
 
     // TODO: Create a method that accepts two integers as parameters and returns their sum
     public int sum(int a, int b) {
-        // Your code here
-        return 0; // Change this line to return the actual sum
+        int ans = a+b
+        return ans; // Change this line to return the actual sum
     }
 
     // TODO: Create a method that accepts two double parameters and returns their product
     public double product(double a, double b) {
-        // Your code here
-        return 0; // Change this line to return the actual product
+        double ans = a*b
+        return ans; // Change this line to return the actual product
     }
 
     // TODO: Create a method that accepts a string parameter and returns it in reverse
     public String reverseString(String str) {
         // Your code here
-        return ""; // Change this line to return the reversed string
+        for(int i = 0;i<5;i++){
+            reversed.num(str.char(i));
+        }
+        return reversed.toString(); // Change this line to return the reversed string
     }
 }
